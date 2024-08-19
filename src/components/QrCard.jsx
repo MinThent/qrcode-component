2836d1967aad1d87e639cc212cffcf49e283ce3b
airfrag <80678006+MinThent@users.noreply.github.com>
2024-08-19 23:09:56 +0900

    added alt tag to img

--- src/components/QrCard.jsx
+++ src/components/QrCard.jsx
@@ -4,7 +4,7 @@ export default function QrCard() {
   return (
     <div className="flex flex-col justify-center bg-white shadow-xl rounded-2xl w-80 text-center">
       <div className="flex justify-center p-3">
-        <img className="rounded-xl w-full max-w-xs" src="image-qr-code.png"></img>
+        <img className="rounded-xl w-full max-w-xs" src="image-qr-code.png" alt="qr-code"></img>
       </div>
       <div className="flex flex-col justify-center p-3 pt-0 w-full">
         <h3 className="p-2 pb-0 font-bold text-2xl text-slate-900">Improve your front-end skills by building projects</h3>
